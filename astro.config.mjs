e16a861c4170c2ce8420f9ebc33d3e5800aae899
Victor DePian <depinav@gmail.com>
2025-09-22 15:49:10 -0400

    Removed base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://depinav.github.io",
-  base: "/depinav.github.io",
   integrations: [mdx(), sitemap()],
 });
